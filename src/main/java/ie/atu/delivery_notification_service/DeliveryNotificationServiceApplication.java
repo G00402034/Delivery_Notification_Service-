eff6e2eb16d81b365bbc2b63e6ebdafd9effb6fd
SeanC <G00402034@atu.ie>
2024-12-20 17:15:26 +0000

    initial commit

---
+++ src/main/java/ie/atu/delivery_notification_service/DeliveryNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.delivery_notification_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryNotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryNotificationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/delivery_notification_service/DeliveryNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.delivery_notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryNotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
